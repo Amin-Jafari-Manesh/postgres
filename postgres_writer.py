b07fd9f6290b579f167209eb8f5e50250b1673a4
Amin Jafari <amincorleone81@gmail.com>
2024-08-07 13:43:15 +0330

    initial

--- postgres_writer.py
+++ postgres_writer.py
@@ -1,32 +1,23 @@
 import logging
+import time
 from os import environ
-from datetime import datetime
 import psycopg2
+from psycopg2 import Error
+from datetime import datetime
 
 logging.basicConfig(level=logging.INFO)
 
 db_config = {
     'PASS': environ.get('PASS', ''),
     'DOMAIN': environ.get('DOMAIN', ''),
-    'HASH_SIZE': int(environ.get('HASH_SIZE', '')),
-    'RECORDS': int(environ.get('RECORDS', '')),
+    'DATA_TYPE': environ.get('DATA_TYPE', ''),
+    'DATA_SIZE': int(environ.get('DATA_SIZE', 1)),
+    'RECORDS': int(environ.get('RECORDS', 100)),
+    'INSERT_DELAY': int(environ.get('INSERT_DELAY', 0)),
 }
 
 
-def generate_random_hash(numb: int = 1) -> str:
-    import random
-    import string
-    import hashlib
-    if numb == 1:
-        return hashlib.sha256(''.join(random.choices(string.ascii_letters + string.digits, k=64)).encode()).hexdigest()
-    else:
-        return ''.join(
-            [hashlib.sha256(''.join(random.choices(string.ascii_letters + string.digits, k=64)).encode()).hexdigest()
-             for _ in range(numb)]
-        )
-
-
-def test_postgres_connection():
+def connect_to_database():
     try:
         conn = psycopg2.connect(
             dbname='db',
@@ -35,43 +26,86 @@ def test_postgres_connection():
             host=db_config['DOMAIN'],
             port='5432'
         )
-        logging.info("PostgreSQL connection successful")
+        if conn:
+            logging.info("Connected to the database.")
+            return conn
+    except Error as e:
+        logging.error(f"Error connecting to the database: {e}")
+    return None
+
+
+def generate_random_hash(numb: int) -> str:
+    import random
+    import string
+    import hashlib
+    return ''.join(
+        [hashlib.sha256(''.join(random.choices(string.ascii_letters + string.digits, k=64)).encode()).hexdigest()
+         for _ in range(numb)])
+
+
+def generate_text(numb: int) -> str:
+    text = ' The quick brown fox jumps over the lazy dog today. '
+    return ''.join([text for _ in range(numb)])
+
+
+def postgres_write_data(conn, size: int) -> bool:
+    if not conn:
+        logging.error("Not connected to the database.")
+        return False
+
+    try:
         cur = conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS hashes (id serial PRIMARY KEY, hash TEXT, created_at TIMESTAMP);")
-        conn.commit()
-        conn.close()
-        return True
-    except Exception as e:
-        logging.error(f"PostgreSQL connection failed: {e}")
-        return False
-
-
-def postgres_write_hash(size: int = 100) -> bool:
-    if test_postgres_connection():
-        try:
-            conn = psycopg2.connect(
-                dbname='db',
-                user='postgres',
-                password=db_config['PASS'],
-                host=db_config['DOMAIN'],
-                port='5432'
-            )
-            cur = conn.cursor()
-            for _ in range(size):
-                cur.execute(
-                    f"INSERT INTO hashes (hash, created_at) VALUES ('{generate_random_hash(db_config['HASH_SIZE'])}', '{datetime.now()}');")
-                conn.commit()
-            conn.close()
-            return True
-        except Exception as e:
-            logging.error(f"Failed to write hashes to the database: {e}")
+        table_name = ''
+        if db_config['DATA_TYPE'] == 'h':
+            table_name = 'hashes'
+            cur.execute("CREATE TABLE IF NOT EXISTS hashes (id SERIAL PRIMARY KEY, hash TEXT, created_at TIMESTAMP);")
+        elif db_config['DATA_TYPE'] == 't':
+            table_name = 'text'
+            cur.execute("CREATE TABLE IF NOT EXISTS text (id SERIAL PRIMARY KEY, texts TEXT, created_at TIMESTAMP);")
+        else:
+            logging.error("Invalid data type.")
             return False
-    else:
+
+        conn.commit()
+
+        for i in range(size):
+            start_time = time.time()
+            if db_config['DATA_TYPE'] == 'h':
+                data = generate_random_hash(db_config['DATA_SIZE'])
+                cur.execute(f"INSERT INTO {table_name} (hash, created_at) VALUES (%s, %s)", (data, datetime.now()))
+            else:
+                data = generate_text(db_config['DATA_SIZE'])
+                cur.execute(f"INSERT INTO {table_name} (texts, created_at) VALUES (%s, %s)", (data, datetime.now()))
+
+            conn.commit()
+            end_time = time.time()
+
+            logging.info(f"Insert execution time: {end_time - start_time:.4f} seconds")
+
+            time.sleep(db_config['INSERT_DELAY'] * 0.001)
+    except Error as e:
+        logging.error(f"Database error: {e}")
         return False
+    finally:
+        if cur:
+            cur.close()
+
+    return True
+
+
+def main():
+    conn = connect_to_database()
+    if conn:
+        if postgres_write_data(conn, db_config['RECORDS']):
+            logging.info("Data successfully written to the database.")
+        else:
+            logging.error("Failed to write data to the database.")
+
+        conn.close()
+        logging.info("Database connection closed.")
+    else:
+        logging.error("Failed to connect to the database.")
 
 
 if __name__ == '__main__':
-    if postgres_write_hash(db_config['RECORDS']):
-        logging.info("Hashes successfully written to the database.")
-    else:
-        logging.error("Failed to write hashes to the database.")
+    main()
